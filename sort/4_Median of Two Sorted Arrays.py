328c7c06190bc28bdb632360765b7c20fc697bd5
Xihang Jiang <84888764+XihangJ@users.noreply.github.com>
2021-07-21 22:02:52 -0400

    Update 4_Median of Two Sorted Arrays.py

--- sort/4_Median of Two Sorted Arrays.py
+++ sort/4_Median of Two Sorted Arrays.py
@@ -44,65 +44,69 @@ class Solution:
     
     
     '''
-    #method 1. binary search -> median O(log(m + n)), S(m + n)
+    #method 1. binary search. O(log(min(m, n)))), S(1)
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if not nums1 and not nums2:
-            return None
+        if not nums1 and not nums2: return
         
-        m, n = len(nums1), len(nums2)
-        if m > n:
-            nums1, nums2, m, n = nums2, nums1, n, m
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        m = len(nums1)
+        n = len(nums2)
+        if m == 0:
+            if n % 2 == 0:
+                return (nums2[(n - 1) // 2] + nums2[n // 2]) / 2
+            else:
+                return float(nums2[n // 2])
             
-        if not nums1:
-            return (nums2[n // 2] + nums2[-1 - n // 2]) / 2
-        
-        if m == 1 and n == 1:
-            return (nums1[0] + nums2[0]) / 2
-        
-        left, right = 0, m
-        while left <= right and left >= 0 and right <= m:
-            i = (left + right) // 2
-            j = (m + n) // 2 - i
+        start, end = 0, m
+        leftSize = (m + n) // 2
+        while start + 1 < end:
+            i = (start + end) // 2
+            j = m - i
+            
             if i == 0:
-                if nums2[j - 1] <= nums1[0] and j < n:
-                    if (m + n) % 2 != 0:
-                        return min(nums1[0], nums2[j])
-                    else:
-                        return (min(nums1[0], nums2[j]) + nums2[j - 1]) / 2
-                elif nums2[j - 1] <= nums1[0] and j == n: 
-                    return (nums2[j-1] + nums1[0]) / 2
-                else:
-                    left = i + 1
-                    print(left)
-            elif j == 0:
-                if nums1[i - 1] <= nums2[0]:
-                    if (m + n) % 2 != 0:
-                        return nums2[0]
-                    else:
-                        return (nums1[i - 1] + nums2[0]) / 2
-                else:
-                    right = i - 1
+                leftMax = nums2[leftSize - 1]
+            elif leftSize == m and i == m:
+                leftMax = nums1[i - 1]
+            else:
+                leftMax = max(nums1[i - 1], nums2[leftSize - i - 1])
                     
-            elif m - i == 0:
-                if j == 0:
-                    if (m + n) % 2 != 0:
-                        return nums2[0]
-                    else:
-                        return (nums1[i - 1] + nums2[0]) / 2
-                else:
-                    if (m + n) % 2 != 0:
-                        return nums2[j]
-                    else:
-                        return (max(nums1[i - 1], nums2[j - 1]) + nums2[j]) / 2
             
-            elif nums1[i - 1] <= nums2[j] and nums2[j - 1] <= nums1[i]:
-                if (m + n) % 2 != 0:
-                    return min(nums1[i], nums2[j])
-                else:
-                    return (min(nums1[i], nums2[j]) + max(nums1[i-1], nums2[j-1])) / 2
-            
-            elif nums1[i - 1] > nums2[j]:
-                right = i - 1
-            elif nums2[j - 1] > nums1[i]:
-                left = i + 1
+            if j == 0:
+                rightMin = nums2[leftSize - i]
+            elif n == m and j == m:
+                rightMin = nums1[0]
+            else:
+                rightMin = min(nums1[i], nums2[leftSize - i])
+                
+            if leftMax <= rightMin:
+                if (m + n) % 2 == 0: return (leftMax + rightMin) / 2
+                else: return float(rightMin)
+            else:
+                if i == 0: start = i
+                elif j == 0: end = i
+                elif nums1[i - 1] > nums2[leftSize - i]: end = i
+                elif nums2[leftSize - i - 1] > nums1[i]: start = i
+        
+        for i in [start, end]:
+            j = m - i
+
+            if i == 0:
+                leftMax = nums2[leftSize - 1]
+            elif leftSize == m and i == m:
+                leftMax = nums1[i - 1]
+            else:
+                leftMax = max(nums1[i - 1], nums2[leftSize - i - 1])
+
+
+            if j == 0:
+                rightMin = nums2[leftSize - i]
+            elif n == m and j == m:
+                rightMin = nums1[0]
+            else:
+                rightMin = min(nums1[i], nums2[leftSize - i])
+                
+            if leftMax <= rightMin:
+                if (m + n) % 2 == 0: return (leftMax + rightMin) / 2
+                else: return float(rightMin)
     '''
