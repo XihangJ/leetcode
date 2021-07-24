862e009a7217555dfeef44088431680bd6d82715
Xihang Jiang <84888764+XihangJ@users.noreply.github.com>
2021-07-24 18:20:43 -0400

    Create 346_Moving Average from Data Stream.py

---
+++ QueueAndStack/346_Moving Average from Data Stream.py
@@ -0,0 +1,22 @@
+from collections import deque
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.size = size
+        self.nums = deque([])
+    def next(self, val: int) -> float:
+        if len(self.nums) < self.size:
+            self.nums.append(val)
+            return sum(self.nums) / len(self.nums)
+        else:
+            self.nums.popleft()
+            self.nums.append(val)
+            return sum(self.nums) / len(self.nums)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
