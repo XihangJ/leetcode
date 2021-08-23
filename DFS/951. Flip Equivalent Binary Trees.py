72fe65ea5c6ff166d84d818be671a9ccfd870bdd
Xihang Jiang <84888764+XihangJ@users.noreply.github.com>
2021-08-23 12:08:14 -0400

    Create 951. Flip Equivalent Binary Trees.py

---
+++ DFS/951. Flip Equivalent Binary Trees.py
@@ -0,0 +1,21 @@
+'''
+For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
+
+A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
+
+Given the roots of two binary trees root1 and root2, return true if the two trees are flip equivelent or false otherwise.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 and not root2: return True
+        elif not root1 or not root2 or root1.val != root2.val: return False
+        
+        return ((self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)) or
+                (self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)))
